84bfb6864ae263434d4ebd3a57cdb6123c108691
Anand Kumar <elanandkumar@gmail.com>
2020-03-19 00:07:56 +0530

    armstrong-numbers: update to use fold method

--- armstrong-numbers/lib/armstrong_numbers.dart
+++ armstrong-numbers/lib/armstrong_numbers.dart
@@ -3,14 +3,16 @@ import 'dart:math';
 class ArmstrongNumbers {
   // Put your code here
   bool isArmstrongNumber(int input) {
-    double sum = 0;
     String inputAsString = input.toString();
-    int len = inputAsString.length;
 
-    for (int i = 0; i < len; i++) {
-      sum += pow(int.parse(inputAsString[i]), len);
-    }
+    int sum = inputAsString.split('').fold(
+      0,
+      (int previous, next) {
+        num nextValue = pow(int.parse(next), inputAsString.length);
+        return (previous + nextValue).toInt();
+      },
+    );
 
-    return sum.toInt() == input;
+    return sum == input;
   }
 }
